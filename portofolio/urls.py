5830537fa610c1fece08a4e803c40d2c6fd13e07
ClevraldoLimuel <clevraldo.limuel@ui.ac.id>
2026-09-01 02:39:27 +0700

    feat:Menambahkan halaman about me
    chore: configure web deployment

--- portofolio/urls.py
+++ portofolio/urls.py
@@ -1,11 +1,11 @@
 """
-URL configuration for myportofolio project.
+URL configuration for portofolio project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.1/topics/http/urls/
 Examples:
 Function views
-    1. Add an import:  from my_app import views
+    1. Add an import:  from _app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 
+from portofolio.views import landing_page
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', landing_page, name='landing_page'),
 ]
