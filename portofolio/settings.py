047fb7dd6a5f8624d6be6f3a39f48e4d4b15ccad
ClevraldoLimuel <clevraldo.limuel@ui.ac.id>
2026-09-07 12:54:06 +0700

    style:compatibilize interest page for thin window (mobile)

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -88,6 +88,7 @@ WSGI_APPLICATION = 'portofolio.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 # Database configuration
+PRODUCTION = False
 if PRODUCTION:
     DATABASES = {
         'default': {
