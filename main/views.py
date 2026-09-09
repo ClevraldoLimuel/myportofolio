870d431e4716452c24b4000b90752a516fc61f85
ClevraldoLimuel <clevraldo.limuel@ui.ac.id>
2026-09-09 14:24:56 +0700

    chore:added test cases
    refactor:added app main
    
    feat:added experience page
    
    chore:linking database

---
+++ main/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Clevraldo Limuel",
+        "npm": "2506656583",
+        "study_program": "S1 Ilmu Komputer",
+        "bio": (
+            "CS student at Universitas Indonesia. Aiming to be a better person. Currently learning much"
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Clevraldo Limuel",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
